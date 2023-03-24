322e19ae0f8f203b5d4a81ab713f1210d365cc88
Jardo <schuppjarod@gmail.com>
2023-03-23 20:32:14 -0400

    Added Home on nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,7 +24,13 @@
 <body class="h-screen w-full flex flex-col overflow-clip">
         <header class="bg-secondary h-12 w-full flex font-FiraCode justify-between text-white">
             <div class="h-12 flex items-center">
-                <h1 class="text-sm pl-4">Jarod Schupp</h1>
+                <div class="h-full w-24 flex justify-center items-center border-navBorder border-x hover:transition-all hover:ease-in-out hover:bg-navHover">
+                    <a id="navText" href="/" class="group">
+                        <span class='bg-left-bottom bg-gradient-to-r from-current to-slate-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'>
+                            Home
+                        </span>
+                    </a>
+                </div>
             </div>
             <nav class="h-12 flex flex-row">
                 <div class="h-full w-24 flex justify-center items-center border-navBorder border-x hover:transition-all hover:ease-in-out hover:bg-navHover">

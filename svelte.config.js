4fc29b4945029f68de8f9267a785ba0125d9081c
Jardo <schuppjarod@gmail.com>
2023-03-27 20:32:43 -0400

    Import error

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,10 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const path = require('path');
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		vite: {
-			resolve: {
-			  alias: {
-				'@': path.resolve('src')
-			  }
-			}
-		  },
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.

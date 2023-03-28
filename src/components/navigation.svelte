9a3c0b3f01c5afe0add46f694dfab33e2ef7f550
Jardo <schuppjarod@gmail.com>
2023-03-28 19:48:10 -0400

    Mobile NavBar Menu Done, default +page.svelte responsivenss done

--- src/components/navigation.svelte
+++ src/components/navigation.svelte
@@ -1,12 +1,15 @@
 <script>
-    import isLoggedIn from '../utilities/isLoggedIn';
+    import isLoggedIn from '../utilities/isLoggedIn.js';
     import { auth } from '../firebase';
+    import { fly } from 'svelte/transition';
  
   
 
 
     function logout() {
-        auth.signOut();
+        auth.signOut().then(() => {
+            window.location.reload();
+        });
     }
 
     let isMenuOpen = false;
@@ -19,39 +22,52 @@
 
 <header class="bg-secondary h-12 w-full flex font-FiraCode justify-between text-white">
     <div class="h-12 flex justify-center items-center  md:hidden lg:hidden xl:hidden 2xl:hidden">
-      <aside>
-        <button id="hamburger-btn" class="p-2 focus:outline-none">
+      <aside class="">
+        <button id="hamburger-btn" class="p-2 focus:outline-none" on:click={toggleMenu} on:mouseleave={toggleMenu}>
           <svg viewBox="0 0 20 20" class="w-6 h-6">
-            <path fill="currentColor" d="M3 4h14a1 1 0 110 2H3a1 1 0 110-2zm0 6h14a1 1 0 110 2H3a1 1 0 110-2zm0 6h14a1 1 0 110 2H3a1 1 0 110-2z"></path>
+              <path fill="currentColor" d="M3 4h14a1 1 0 110 2H3a1 1 0 110-2zm0 6h14a1 1 0 110 2H3a1 1 0 110-2zm0 6h14a1 1 0 110 2H3a1 1 0 110-2z"></path>
           </svg>
         </button>
         {#if isMenuOpen}
-        <nav id="menu" class="hidden absolute bg-secondary top-12 left-0 w-full p-4 z-10">
-          <ul class="text-white">
-            <li><a href="#">Menu Item 1</a></li>
-            <li><a href="#">Menu Item 2</a></li>
-            <li><a href="#">Menu Item 3</a></li>
-          </ul>
-        </nav>
-        {/if}
-      </aside>
-    </div>
-    <div class="h-12 flex items-center sm:hidden">
-      <div
-        class="h-full w-24 flex justify-center items-center border-navBorder border-x hover:transition-all hover:ease-in-out hover:bg-navHover"
-      >
-        <a id="" href="/" class="group">
-          <span
-            class='bg-left-bottom bg-gradient-to-r from-current to-slate-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'
-          >
-            Home
-          </span>
-        </a>
-      </div>
-  
-      {#if $isLoggedIn}
+        <nav id="menu" class="absolute bg-secondary w-44 h-fit border-t shadow-2xl overflow-clip" in:fly="{{ x: -200, duration: 2000 }}" out:fly="{{ x: -200, duration: 2000 }}">
+          <div class="h-12 w-full flex justify-center items-center border-navBorder border-x hover:transition-all hover:ease-in-out hover:bg-navHover">
+            <a id="" href="/about" class="group">
+                <span class='bg-left-bottom bg-gradient-to-r from-current to-slate-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'>
+                    About
+                </span>
+            </a>
+        </div>
+        <div class="h-12 w-full flex justify-center items-center border-navBorder border hover:bg-navHover">
+            <a id="" href="/projects" class="group">
+                <span class='bg-left-bottom bg-gradient-to-r from-current to-slate-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'>
+                    Projects
+                </span>
+            </a>
+        </div>
+        <div class="h-12 w-full flex justify-center items-center border-navBorder border-b hover:bg-navHover">
+            <a id="" href="/contact" class="group">
+                <span class='bg-left-bottom bg-gradient-to-r from-current to-slate-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'>
+                    Contact
+                </span>
+            </a>
+        </div>
+        <div class="h-12 w-full flex justify-center items-center border-navBorder border-b hover:bg-navHover">
+            <a id="" href="/resume" class="group">
+                <span class='bg-left-bottom bg-gradient-to-r from-current to-slate-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'>
+                    Resume
+                </span>
+            </a>
+        </div>
+        <div class="h-12 w-full flex justify-center items-center border-navBorder border-b hover:bg-navHover">
+            <a id="" href="/uses" class="group">
+                <span class='bg-left-bottom bg-gradient-to-r from-current to-slate-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'>
+                    Uses
+                </span>
+            </a>
+        </div>
+        {#if $isLoggedIn}
         <div
-          class="h-full w-24 flex justify-center items-center border-navBorder border-x hover:transition-all hover:ease-in-out hover:bg-navHover"
+          class="h-12 w-full flex justify-center items-center border-navBorder border-b hover:transition-all hover:ease-in-out hover:bg-navHover"
         >
           <a id="" href="/admin" class="group">
             <span
@@ -62,8 +78,8 @@
           </a>
         </div>
         <div
-          class="h-full w-24 flex justify-center items-center border-navBorder border-x hover:transition-all hover:ease-in-out hover:bg-navHover cursor-pointer"
-          on:click={logout} on:keyDown={logout}
+          class="h-12 w-full flex justify-center items-center border-navBorder border-b hover:transition-all hover:ease-in-out hover:bg-navHover cursor-pointer"
+          on:click={logout} on:keydown={logout}
         >
           <span
             class='bg-left-bottom bg-gradient-to-r from-current to-slate-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'
@@ -73,7 +89,7 @@
         </div>
       {:else}
         <div
-          class="h-full w-24 flex justify-center items-center border-navBorder border-x hover:transition-all hover:ease-in-out hover:bg-navHover"
+          class="h-12 w-full flex justify-center items-center border-navBorder border-b hover:transition-all hover:ease-in-out hover:bg-navHover"
         >
           <a id="" href="/login" class="group">
             <span
@@ -84,38 +100,90 @@
           </a>
         </div>
       {/if}
+        </nav>
+        {/if}
+      </aside>
+    </div>
+    <div class="h-12 flex items-center sm:hidden">
+      <div
+        class="h-12 w-full flex justify-center items-center border-navBorder border-x hover:transition-all hover:ease-in-out hover:bg-navHover"
+      >
+        <a id="" href="/" class="group w-24 flex justify-center items-center">
+          <span
+            class='bg-left-bottom bg-gradient-to-r from-current to-slate-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'
+          >
+            Home
+          </span>
+        </a>
+      </div>
+  
+      {#if $isLoggedIn}
+        <div
+          class="h-12 w-full flex justify-center items-center border-navBorder border-x hover:transition-all hover:ease-in-out hover:bg-navHover"
+        >
+          <a id="" href="/admin" class="group">
+            <span
+              class='bg-left-bottom bg-gradient-to-r from-current to-slate-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'
+            >
+              Panel
+            </span>
+          </a>
+        </div>
+        <div
+          class="h-12 w-full flex justify-center items-center border-navBorder border-x hover:transition-all hover:ease-in-out hover:bg-navHover cursor-pointer"
+          on:click={logout} on:keydown={logout}
+        >
+          <span
+            class='bg-left-bottom bg-gradient-to-r from-current to-slate-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'
+          >
+            Logout
+          </span>
+        </div>
+      {:else}
+        <div
+          class="h-12 w-full flex justify-center items-center border-navBorder border-x hover:transition-all hover:ease-in-out hover:bg-navHover"
+        >
+          <a id="" href="/login" class="group w-24 flex justify-center items-center">
+            <span
+              class='bg-left-bottom bg-gradient-to-r from-current to-slate-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'
+            >
+              Login
+            </span>
+          </a>
+        </div>
+      {/if}
     </div>
     <nav class="h-12 flex flex-row sm:hidden">
-        <div class="h-full w-24 flex justify-center items-center border-navBorder border-x hover:transition-all hover:ease-in-out hover:bg-navHover">
-            <a id="" href="/about" class="group">
+        <div class="h-12 w-full flex justify-center items-center border-navBorder border-x hover:transition-all hover:ease-in-out hover:bg-navHover">
+            <a id="" href="/about" class="group w-24 flex justify-center items-center">
                 <span class='bg-left-bottom bg-gradient-to-r from-current to-slate-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'>
                     About
                 </span>
             </a>
         </div>
-        <div class="h-full w-24 flex justify-center items-center border-navBorder border-r hover:bg-navHover">
-            <a id="" href="/projects" class="group">
+        <div class="h-12 w-full flex justify-center items-center border-navBorder border-r hover:bg-navHover">
+            <a id="" href="/projects" class="group w-24 flex justify-center items-center">
                 <span class='bg-left-bottom bg-gradient-to-r from-current to-slate-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'>
                     Projects
                 </span>
             </a>
         </div>
-        <div class="h-full w-24 flex justify-center items-center border-navBorder border-r hover:bg-navHover">
-            <a id="" href="/contact" class="group">
+        <div class="h-12 w-full flex justify-center items-center border-navBorder border-r hover:bg-navHover">
+            <a id="" href="/contact" class="group w-24 flex justify-center items-center">
                 <span class='bg-left-bottom bg-gradient-to-r from-current to-slate-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'>
                     Contact
                 </span>
             </a>
         </div>
-        <div class="h-full w-24 flex justify-center items-center border-navBorder border-r hover:bg-navHover">
-            <a id="" href="/resume" class="group">
+        <div class="h-12 w-full flex justify-center items-center border-navBorder border-r hover:bg-navHover">
+            <a id="" href="/resume" class="group w-24 flex justify-center items-center">
                 <span class='bg-left-bottom bg-gradient-to-r from-current to-slate-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'>
                     Resume
                 </span>
             </a>
         </div>
-        <div class="h-full w-24 flex justify-center items-center border-navBorder border-r hover:bg-navHover">
-            <a id="" href="/uses" class="group">
+        <div class="h-12 w-full flex justify-center items-center border-navBorder border-r hover:bg-navHover">
+            <a id="" href="/uses" class="group w-24 flex justify-center items-center">
                 <span class='bg-left-bottom bg-gradient-to-r from-current to-slate-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'>
                     Uses
                 </span>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,12 +11,12 @@
     <title>Jarod Schupp</title>
 </svelte:head>
 
-<section in:fade="{{duration: 2000}}" class="bg-no-repeat h-screen w-full justify-between font-FiraCode text-white flex flex-row px-12 animate-fade-in">
-    <section class="flex flex-col justify-center w-1/2">
-        <span class="text-8xl"> 
+<section in:fade="{{duration: 2000}}" class="h-screen w-full justify-between items-center font-FiraCode text-white flex flex-row px-12 animate-fade-in sm:flex-col sm:items-center sm:px-0 sm:justify-around md:flex-col md:items-center md:px-0 md:justify-around lg:flex-col lg:items-center lg:px-0 lg:justify-around">
+    <section class="flex flex-col justify-center w-1/2 h-1/2 sm:w-full sm:mt-12">
+        <span class="text-8xl sm:text-5xl sm:items-center sm:justify-center sm:flex md:text-5xl md:items-center md:justify-center md:flex lg:text-6xl lg:items-center lg:justify-center lg:flex"> 
             <RandomLetters /> 
         </span>
-        <h2 class="text-5xl">Software Engineer</h2>
+        <h2 class="text-5xl sm:text-3xl sm:items-center sm:justify-center sm:flex md:text-3xl md:items-center md:justify-center md:flex lg:text-4xl lg:items-center lg:justify-center lg:flex">Software Engineer</h2>
     </section>
-    <img class="mx-40" src="undrawCode.svg" alt="undrawCode" height="500px" width="500px">
+    <img class="mx-12" src="undrawCode.svg" alt="undrawCode" height="500px" width="500px">
 </section>
